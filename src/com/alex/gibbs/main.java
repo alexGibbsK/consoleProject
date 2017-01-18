7174fe9f1c08eaac930a0ebe491972cdf1593192
alexGibbsK <alex.gibbs.k@gmail.com>
2017-01-18 14:46:55 +0200

    124

--- src/com/alex/gibbs/main.java
+++ src/com/alex/gibbs/main.java
@@ -2,6 +2,6 @@ package com.alex.gibbs;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello.");
+        System.out.println("Hello World");
     }
 }
